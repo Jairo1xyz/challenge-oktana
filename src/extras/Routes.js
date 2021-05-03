ce81927e2ca7c066167b4f94efa92012f9a638f6
Jairo Collao <illapu1xyz@gmail.com>
2021-05-03 02:09:55 -0500

    cleaned

--- src/extras/Routes.js
+++ src/extras/Routes.js
@@ -3,8 +3,6 @@ import { Router, Switch, Route } from "react-router-dom";
 
 import Home from "../components/Home";
 import Calculator from "../components/Calculator";
-import Sample1 from "../components/Sample1";
-import Sample2 from "../components/Sample2";
 import history from './history';
 
 export default class Routes extends Component {
@@ -14,8 +12,6 @@ export default class Routes extends Component {
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/Calculator" exact component={Calculator} />
-                    <Route path="/Sample1" component={Sample1} />
-                    <Route path="/Sample2" component={Sample2} />
                 </Switch>
             </Router>
         )
